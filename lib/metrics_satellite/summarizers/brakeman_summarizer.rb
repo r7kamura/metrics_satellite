5eb291f0c9fb241f158f29c096e5e4dbbe61c01c
Ryo Nakamura <ryo-nakamura@cookpad.com>
2012-12-20 13:40:34 +0900

    Create BrakemanSummarizer

--- lib/metrics_satellite/summarizers/brakeman_summarizer.rb
+++ lib/metrics_satellite/summarizers/brakeman_summarizer.rb
@@ -1,5 +1,35 @@
+require "nokogiri"
+
 module MetricsSatellite
   class BrakemanSummarizer < Summarizer
     EXT = "html"
+
+    def summarize
+      Summary.new(:count => count)
+    end
+
+    private
+
+    def count
+      count_node.text.strip.to_i
+    end
+
+    def count_node
+      count_node_parent.css("td")[1]
+    end
+
+    def count_node_parent
+      tree.css("#summary + table > tbody > tr").detect do |tr|
+        tr.css("td")[0].text =~ /Security Warnings/
+      end
+    end
+
+    def tree
+      Nokogiri.HTML(content)
+    end
+
+    def content
+      pathname.read
+    end
   end
 end
---
+++ spec/metrics_satellite/summarizers/brakeman_summarizer_spec.rb
@@ -0,0 +1,56 @@
+require "spec_helper"
+
+module MetricsSatellite
+  describe BrakemanSummarizer do
+    let(:summarizer) do
+      described_class.new
+    end
+
+    describe "#summarize" do
+      subject do
+        summarizer.summarize
+      end
+
+      before do
+        summarizer.stub(:pathname => pathname)
+      end
+
+      let(:pathname) do
+        mock(:exist? => true, :read => content)
+      end
+
+      let(:content) do
+        <<-EOF
+          <!DOCTYPE HTML>
+          <html lang="ja">
+          <head>
+          </head>
+          <body>
+            <h2 id="summary"></h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Scanned/Reported</td>
+                  <td>total</td>
+                </tr>
+                <tr>
+                  <td>Security Warnings</td>
+                  <td>#{count}</td>
+                </tr>
+              </tbody>
+            </table>
+          </body>
+          </html>
+        EOF
+      end
+
+      let(:count) do
+        100
+      end
+
+      it "creates summary of Brakeman" do
+        subject.count.should == count
+      end
+    end
+  end
+end
